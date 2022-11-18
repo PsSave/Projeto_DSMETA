60a8f838fa5bd1c37675cdb196f45095df95debb
Pedro Samuel <pedrosviki1265@gmail.com>
2022-11-18 16:24:52 -0400

    att

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -14,7 +14,7 @@ function SalesCard() {
   const [maxDate, setMaxDate] = useState(max);
 
   useEffect(() => {
-    axios.get("https://dsmeta-pedrosamuel.herokuapp.com/sales").then(response => {
+    axios.get("http://localhost:8080/sales").then(response => {
       console.log(response.data)
     })
   }, [])
